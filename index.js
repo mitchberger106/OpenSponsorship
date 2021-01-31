2a41fea3b33e07922a33d74200b1233597355479
Mitchell Berger <mitchberger106@gmail.com>
2021-01-31 18:50:26 -0500

    t

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ if (process.env.NODE_ENV === 'production') {
 
   const path = require('path');
   app.get('*', (req,res) => {
-      res.sendFile(path.resolve(__dirname, '/opensponsorship', 'build', 'index.html'))
+      res.sendFile(path.resolve(__dirname, 'opensponsorship', 'build', 'index.html'))
   })
 
 }
