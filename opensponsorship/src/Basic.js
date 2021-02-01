5f090d228ea6347228a1e871406f1989933a9c68
Mitchell Berger <mitchberger106@gmail.com>
2021-02-01 12:43:44 -0500

    dependencies

--- opensponsorship/src/Basic.js
+++ opensponsorship/src/Basic.js
@@ -2,10 +2,8 @@ import React from "react";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form'
 
-import ItemForm from "./ItemForm";
-
 const Basic = ({ setForm, formData, navigation }) => {
-  const { firstName, lastName, nickName } = formData;
+  const { name, sport, gender, dateOfBirth } = formData;
 
   const { next } = navigation;
 
@@ -13,27 +11,27 @@ const Basic = ({ setForm, formData, navigation }) => {
     <Form>
   <Form.Group controlId="formBasicName">
     <Form.Label>Name</Form.Label>
-    <Form.Control type="name" placeholder="Enter name" />
+    <Form.Control value={name} type="name" placeholder="Enter name" />
   </Form.Group>
 
   <Form.Group controlId="formBasicSport">
     <Form.Label>Sport</Form.Label>
-    <Form.Control type="sport" placeholder="Select Sport" />
+    <Form.Control value={sport} type="sport" placeholder="Select Sport" />
   </Form.Group>
 
   <Form.Group controlId="formBasicGender">
     <Form.Label>Gender</Form.Label>
-    <Form.Control as="select">
+    <Form.Control value={gender} as="select">
       <option>Male</option>
       <option>Female</option>
       <option>Other</option>
     </Form.Control>
   </Form.Group>
   <Form.Group controlId="formBasicDateOfBirth">
-    <Form.Label>Name</Form.Label>
-    <Form.Control type="dateOfBirth" placeholder="Enter date of birth" />
+    <Form.Label>Date of birth</Form.Label>
+    <Form.Control value={dateOfBirth} type="dateOfBirth" placeholder="Enter date of birth" />
   </Form.Group>
-  <Button variant="primary" type="submit">
+  <Button onClick={next} variant="primary" type="submit">
     Next
   </Button>
 </Form>
